cca362d9fb030956c18a6678e1b04222cf9ddea9
TApicella <tapicella.ms@gmail.com>
2015-03-18 21:31:46 -0400

    Controls all added in frames and refactored

--- sorting_controls.py
+++ sorting_controls.py
@@ -8,13 +8,25 @@ from sys import exit
 
 class SortingControls:
 	def __init__(self, app, parent):
-		self.skip_btn = Button(parent, text="Skip", command=app.next)
-		self.edited_btn = Button(parent, text="Already edited", command=lambda: self.write_comment(app, app.edited))
-		self.edit_btn = Button(parent, text="I like this one!", command=lambda: self.write_comment(app, app.to_edit))
-		self.veto_btn = Button(parent, text="I don't like this one :(", command= lambda: self.write_comment(app, app.veto))
+	
+		self.sc_frame = Frame(parent, height=350, width=400, bd=5, relief=RAISED)
+		self.sc_frame.pack_propagate(False) 
+		#Don't pack until editing has started
 		
-		self.comment_label = Label(parent, text="Add a comment")
-		self.comment_box = Entry(parent)
+		self.skip_btn = Button(self.sc_frame, text="Skip", command=app.next)
+		self.edited_btn = Button(self.sc_frame, text="Already edited", command=lambda: self.write_comment(app, app.edited))
+		self.edit_btn = Button(self.sc_frame, text="I like this one!", command=lambda: self.write_comment(app, app.to_edit))
+		self.veto_btn = Button(self.sc_frame, text="I don't like this one :(", command= lambda: self.write_comment(app, app.veto))
+		
+		self.skip_btn.pack()
+		self.edited_btn.pack()
+		self.edit_btn.pack()
+		self.veto_btn.pack()
+		
+		self.comment_label = Label(self.sc_frame, text="Add a comment")
+		self.comment_box = Entry(self.sc_frame)
+		self.comment_label.pack()
+		self.comment_box.pack()
 	
 	def write_comment(self, app, fname, event=None):
 		with open(fname, "a") as f:
@@ -24,18 +36,11 @@ class SortingControls:
 			f.write(app.current_photo+"\n\n")
 		app.next()
 		
-	def grid(self, appgrid):	
-		self.skip_btn.grid(column=appgrid["skip_btn_col"], row=appgrid["skip_btn_row"])
-		self.edited_btn.grid(column=appgrid["edited_btn_col"], row=appgrid["edited_btn_row"])
-		self.edit_btn.grid(column=appgrid["edit_btn_col"], row=appgrid["edit_btn_row"])
-		self.veto_btn.grid(column=appgrid["veto_btn_col"], row=appgrid["veto_btn_row"])
-		self.comment_label.grid(column=appgrid["comment_label_col"], row=appgrid["comment_label_row"])
-		self.comment_box.grid(column=appgrid["comment_box_col"], row=appgrid["comment_box_row"])
-		
 	def delete_comment(self):
 		self.comment_box.delete(0, END)
 		
-	
+	def pack(self):
+		self.sc_frame.pack()
 	
 	
 	
